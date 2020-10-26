374882cd620b83dd430287209b53ef47b52418a1
Guilherme Pohlmann <guilherme.pohlmann@sap.com>
2020-10-26 19:10:05 -0300

    Refactoring

--- backend/service/authentication/impl/DefaultTokenGenerationService.ts
+++ backend/service/authentication/impl/DefaultTokenGenerationService.ts
@@ -2,16 +2,11 @@ import { TokenGenerationService } from "../TokenGenerationService";
 
 import { TokenGenerationRequestUser } from './../model/TokenGenerationRequestUser';
 import { TokenGenerationRequest } from './../model/TokenGenerationRequest';
+import { DonationsTrackerConstants } from "../../../constants/DonationsTrackerConstants";
 
 const request = require('request');
 
 export class DefaultTokenGenerationService implements TokenGenerationService {
-    readonly TOKEN_SERVICE_ENDPOINT: string = "https://api.globalgiving.org/api/userservice/tokens";
-    readonly TOKEN_GENERATION_USER: string = "testguipohl@gmail.com";
-    readonly TOKEN_GENERATION_PASSWORD: string = "Donationstrackerpass";
-    readonly TOKEN_GENERATION_API_KEY: string = "0909f816-07cf-4587-8e51-41745d8edef6";
-
-
     async generate(): Promise<string> {
         return this.doRequest();
     }
@@ -19,15 +14,16 @@ export class DefaultTokenGenerationService implements TokenGenerationService {
     private async doRequest(): Promise<string> {
         return new Promise((resolve, reject) => {
             const authenticationUser: TokenGenerationRequestUser =
-                new TokenGenerationRequestUser(this.TOKEN_GENERATION_USER, this.TOKEN_GENERATION_PASSWORD);
+                new TokenGenerationRequestUser(DonationsTrackerConstants.GLOBAL_GIVING_USER, 
+                    DonationsTrackerConstants.GLOBAL_GIVING_PASSWORD);
             const authenticationRequest: TokenGenerationRequest =
-                new TokenGenerationRequest(authenticationUser, this.TOKEN_GENERATION_API_KEY);
+                new TokenGenerationRequest(authenticationUser, DonationsTrackerConstants.API_KEY);
 
             const bodyData: string = JSON.stringify({ "auth_request": authenticationRequest });
 
             request.post({
                 headers: { 'accept': 'application/json' },
-                url: this.TOKEN_SERVICE_ENDPOINT,
+                url: DonationsTrackerConstants.TOKEN_SERVICE_ENDPOINT,
                 body: bodyData
             }, function (error: any, response: any, body: any) {
                 const bodyResponse: any = JSON.parse(body);
--- backend/service/donation/impl/DefaultDonationService.ts
+++ backend/service/donation/impl/DefaultDonationService.ts
@@ -1,3 +1,4 @@
+import { DonationsTrackerConstants } from "../../../constants/DonationsTrackerConstants";
 import { DefaultTokenGenerationService } from "../../authentication/impl/DefaultTokenGenerationService";
 import { TokenGenerationService } from "../../authentication/TokenGenerationService";
 import { DonationService } from "../DonationService";
@@ -5,12 +6,7 @@ import { SubmitDonationRequest } from "../model/SubmitDonationRequest";
 const request = require('request');
 const { v4: uuidv4 } = require('uuid');
 
-export class DefaultDonationService implements DonationService {
-    readonly DONATION_SERVICE_ENDPOINT: string = "https://api.globalgiving.org/api/secure/givingservice/donationsclient?is_test=true";
-    readonly API_KEY: string = "0909f816-07cf-4587-8e51-41745d8edef6";
-    readonly PAYMENT_GATEWAY: string = "braintree";
-    readonly PAYMENT_GATEWAY_KEY: string = "12345";
-    
+export class DefaultDonationService implements DonationService {    
     tokenGenerationService: TokenGenerationService;
 
     constructor() {
@@ -20,14 +16,15 @@ export class DefaultDonationService implements DonationService {
     async submitDonation(submitDonationRequest: SubmitDonationRequest) {
         let authenticationToken: string = await this.tokenGenerationService.generate();
         submitDonationRequest.refcode = uuidv4();
-        submitDonationRequest.payment_detail.paymentGateway = this.PAYMENT_GATEWAY;
-        submitDonationRequest.payment_detail.paymentGatewayKey = this.PAYMENT_GATEWAY_KEY;
+        submitDonationRequest.payment_detail.paymentGateway = DonationsTrackerConstants.PAYMENT_GATEWAY;
+        submitDonationRequest.payment_detail.paymentGatewayKey = DonationsTrackerConstants.PAYMENT_GATEWAY_KEY;
 
         const bodyData: string = JSON.stringify({ "donation": submitDonationRequest });
 
         await request.post({
             headers: { 'accept': 'application/json' },
-            url: this.DONATION_SERVICE_ENDPOINT + "api_key=" + this.API_KEY + "&api_token=" + authenticationToken,
+            url: DonationsTrackerConstants.DONATION_SERVICE_ENDPOINT + "api_key=" 
+            + DonationsTrackerConstants.API_KEY + "&api_token=" + authenticationToken,
             body: bodyData
         }, function (error: any, response: any, body: any) {
             const bodyResponse: any = JSON.parse(body);
