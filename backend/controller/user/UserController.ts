78ea84b88d736f4e2535cb1a959a90adaf06fa16
Guilherme Pohlmann <guilherme.pohlmann@sap.com>
2020-11-15 15:57:10 -0300

    Adding login/logout to user controller

--- backend/controller/user/UserController.ts
+++ backend/controller/user/UserController.ts
@@ -4,20 +4,53 @@ import express from 'express';
 import { UserFacade } from "../../facade/user/UserFacade";
 import { DefaultUserFacade } from "../../facade/user/impl/DefaultUserFacade";
 import { UserDTO } from "../../facade/dto/UserDTO";
+import { AbstractController } from "../AbtractController";
 
-class UserController implements Controller {
+const jwt = require('jsonwebtoken');
+
+class UserController extends AbstractController implements Controller {
     readonly CONTEXT_PATH: string = "/user"
     userFacade: UserFacade;
     router: express.Router;
 
     constructor() {
+        super();
         this.router = express.Router();
         this.router.post('', this.create.bind(this));
         this.router.put('', this.update.bind(this));
         this.router.delete('', this.delete.bind(this));
         this.router.get('', this.get.bind(this));
         this.router.get('/all', this.getAll.bind(this));
+        this.router.post('/login', this.login.bind(this));
+        this.router.post('/logout', this.logout.bind(this));
         this.userFacade = new DefaultUserFacade();
+    }
+
+    public async login(request: Request, response: Response) {
+        this.validateBody(request, response);
+        try {
+            var userDTO: UserDTO = await this.userFacade.login(request.body.email, request.body.password);
+            var token = jwt.sign({id: userDTO.email}, "AUTH_SECRET_DONATIONS_TRACKER", {
+                expiresIn: 1200
+            });
+            userDTO.isAuthenticated = true;
+            userDTO.token = token;
+            response.send(userDTO);
+        } catch (error) {
+            response.status(400).send("Login failed, cause: " + error)
+        }
+    }
+
+    public async logout(request: Request, response: Response) {
+        this.validateBody(request, response);
+        try {
+            var userDTO: UserDTO = await this.userFacade.get(request.body.email);
+            userDTO.token = null;
+            userDTO.isAuthenticated = false;
+            response.send(userDTO);
+        } catch (error) {
+            response.status(400).send("Unable to log out, cause: " + error)
+        }
     }
 
     public create(request: Request, response: Response) {
