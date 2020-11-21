a74006090337845b8373f845e394ee7ae558cc94
Guilherme Pohlmann <guilherme.pohlmann@sap.com>
2020-11-21 17:05:54 -0300

    Attempt to fix cors

--- backend/Server.ts
+++ backend/Server.ts
@@ -27,13 +27,7 @@ class Server {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-        
-            if ('OPTIONS' == req.method) {
-              res.send(200);
-            }
-            else {
-              next();
-            }
+            next();
         });
         this.application.use(bodyParser.json());
         this.application.use(bodyParser.urlencoded({ extended: true }));
