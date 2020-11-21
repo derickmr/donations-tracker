131c299303703cf5a559d9f462823f4458ee6a41
Derick Rentz <derick.rentz@sap.com>
2020-11-21 16:36:11 -0300

    Setting token to request header

--- frontend/src/service/index.ts
+++ frontend/src/service/index.ts
@@ -4,8 +4,10 @@ import { User } from '../pages/user-details/types'
 const requestAxios = axios.create({
   baseURL: 'https://donations-tracker-backend.herokuapp.com/',
   timeout: 10000,
-  headers: { 'Content-Type': 'application/json' },
-})
+  headers: { 'Content-Type': 'application/json',
+            'x-access-token': localStorage.getItem("token")
+  },
+});
 
 export class Api {
   static getONGs() {
